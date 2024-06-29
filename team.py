ff36fd60fb8097756916694fb2be63acee8b1d1c
leeyounghuncom <web3dao@daum.net>
2024-06-29 13:41:25 +0900

    예제코드 복붙

---
+++ team.py
@@ -0,0 +1,22 @@
+
+#예제 코드 복붙 이렇게 해서 작업 시작 해보자
+
+# ----- 코드 정의 ------
+class Member:
+    # TODO : 코드 구현이 필요합니다.
+
+    def display(self):
+        # TODO : 코드 구현이 필요합니다.
+        pass
+
+
+class Post:
+    # TODO : 코드 구현이 필요합니다.
+    pass
+
+
+# ----- 코드 실행 ------
+members = []
+posts = []
+
+# TODO : 코드 구현이 필요합니다.
