84a5aa09a77327508aea94518ba92a18970e272b
leeyounghuncom <web3dao@daum.net>
2024-06-29 15:06:30 +0900

    멤버 인스턴스

--- team.py
+++ team.py
@@ -1,9 +1,17 @@
-
 #예제 코드 복붙 이렇게 해서 작업 시작 해보자
 
 # ----- 코드 정의 ------
+# 1. Member 클래스와 Post 클래스를 정의하세요.
 class Member:
-    # TODO : 코드 구현이 필요합니다.
+    # 코드 구현이 필요합니다.
+    # Member 클래스에는 다음과 같은 속성을 가지고 있어야 합니다.
+    #     - 회원 이름 (**`name`**)
+    #     - 회원 아이디 (**`username`**)
+    #     - 회원 비밀번호 (**`password`**)
+    def __init__(self, name, username, password):
+        self.name = name
+        self.username = username
+        self.password = password
 
     def display(self):
         # TODO : 코드 구현이 필요합니다.
